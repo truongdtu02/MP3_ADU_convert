0f7c69849e7be5bf9b4e5d2c1dd5a9bee4f444f1
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-06-30 16:33:23 +0700

    stream udp

--- FFmpegLinux/FFmpegXabe.cs
+++ FFmpegLinux/FFmpegXabe.cs
@@ -13,6 +13,8 @@ namespace FFmpegLinux
     public class FFmpegXabe
     {
         //public static string curPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static int count = 0;
+        public static FileStream fs;// = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
         public async Task convertMP3(string pathFile, string nameFile)
         {
             //Get latest version of FFmpeg. It's great idea if you don't know if you had installed FFmpeg.
@@ -21,6 +23,7 @@ namespace FFmpegLinux
             pathFile = Path.Combine(pathFile, nameFile);
             string outPath = Path.Combine(@"Data2", nameFile);
             outPath = Path.ChangeExtension(outPath, ".mp3");
+            //fs = new FileStream(outPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
 
             //string outPath = "converted"; // Your code goes here
             //outPath = Path.Combine(curPath, outPath, nameFile);
@@ -41,26 +44,33 @@ namespace FFmpegLinux
                 ;
             //Create new conversion object
             var conversion = FFmpeg.Conversions.New()
+                .AddParameter("-re", ParameterPosition.PreInput)
                 //Add audio stream to output file
                 .AddStream(audioStream)
                 //Set output file path
-                .SetOutput(outPath)
+                //.SetOutput(outPath)
                 //SetOverwriteOutput to overwrite files. It's useful when we already run application before
-                .SetOverwriteOutput(true)
+                //.SetOverwriteOutput(true)
                 //Disable multithreading
                 .UseMultiThread(true)
                 //Set conversion preset. You have to chose between file size and quality of video and duration of conversion
-                .SetPreset(ConversionPreset.UltraFast);
+                .SetPreset(ConversionPreset.UltraFast)
+            .AddParameter("-f mp3 udp://127.0.0.1:11000");
             //Add log to OnProgress
-            //conversion.OnProgress += async (sender, args) =>
-            //{
-            //        //Show all output from FFmpeg to console
-            //        await Console.Out.WriteLineAsync($"[{args.Duration}/{args.TotalLength}][{args.Percent}%] {nameFile}");
-            //};
-            conversion.OnDataReceived += (sender, args) =>
+            conversion.OnProgress += async (sender, args) =>
             {
-                Console.WriteLine($"{args.Data}{Environment.NewLine}");
+                //Show all output from FFmpeg to console
+                await Console.Out.WriteLineAsync($"[{args.Duration}/{args.TotalLength}][{args.Percent}%] {nameFile}");
+                count++;
             };
+
+            //conversion.OnProgress += (duration, length) => { currentProgress = duration; }
+
+            //conversion.OnDataReceived += (sender, args) =>
+            //{
+            //    Console.WriteLine($"{args.Data}{sender.ToString()}");
+            //    count++;
+            //};
             //Start conversion
             await conversion.Start();
 
--- FFmpegLinux/Program.cs
+++ FFmpegLinux/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -21,12 +21,22 @@ namespace FFmpegLinux
             string path1 = @"Data";
             string path2 = @"Data2";
             //copy file to multiple version
-            for (int i = 1; i <= numOfProcess; i++)
-            {
-                File.Copy(Path.Combine(path1, fileName + ".mp3"), Path.Combine(path1, fileName + i.ToString() + ".mp3"), true);
-            }
-            //var ffmpegxabe = new FFmpegXabe();
-            //await ffmpegxabe.convertMP3("Data", "test" + ".mp3");
+            //for (int i = 1; i <= numOfProcess; i++)
+            //{
+            //    File.Copy(Path.Combine(path1, fileName + ".mp3"), Path.Combine(path1, fileName + i.ToString() + ".mp3"), true);
+            //}
+            //Thread tStream = new Thread(() => {
+            //    while (FFmpegXabe.count < 2) ;
+
+            //    //new strea file
+            //    var readMp3 = new FileStream(Path.Combine(path2, fileName + "mp3"), FileMode.Open, FileAccess.Read);
+            //    byte[] newBuff = new byte[10];
+            //    readMp3.Read(newBuff, 0, 10);
+            //    Console.WriteLine("read done");
+            //});
+            //tStream.Start();
+            var ffmpegxabe = new FFmpegXabe();
+            await ffmpegxabe.convertMP3("Data", "trường bờm hị hí hỉ hĩ" + ".mp3");
             //for (int i = 1; i <= numOfProcess; i++)
             //{
             //    await FFmpegXabe.convertMP3("Data", fileName + i.ToString() + ".mp3");
@@ -34,20 +44,19 @@ namespace FFmpegLinux
             //}
 
             //await FFmpegXabe.convertMP3("Data", "test" + ".mp3");
-            await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official);
+            //await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official);
 
-            for (int i = 1; i <= numOfProcess; i++)
-            {
-                string iTmp = i.ToString();
-                Thread t = new Thread(async () =>
-                {
-                    var ffmpegxabe = new FFmpegXabe();
-                    await ffmpegxabe.convertMP3("Data", "test" + iTmp + ".mp3");
-                });
-                t.Start();
-                //File.Copy(Path.Combine(path1, fileName + ".mp3"), Path.Combine(path1, fileName + i.ToString() + ".mp3"), true);
-            }
-
+            //for (int i = 1; i <= numOfProcess; i++)
+            //{
+            //    string iTmp = i.ToString();
+            //    Thread t = new Thread(async () =>
+            //    {
+            //        var ffmpegxabe = new FFmpegXabe();
+            //        await ffmpegxabe.convertMP3("Data", "test" + iTmp + ".mp3");
+            //    });
+            //    t.Start();
+            //    //File.Copy(Path.Combine(path1, fileName + ".mp3"), Path.Combine(path1, fileName + i.ToString() + ".mp3"), true);
+            //}
             Console.In.ReadLine();
         }
         public static async Task Main(string[] args)
